4e6fd87302f0ebe884d3f246d6f3f852722ad9fb
RZLA16 <rstar026@yahoo.com>
2017-08-20 21:48:31 -0700

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('hamburger');
+INSERT INTO burgers (burger_name) VALUES ('cheese burger');
+INSERT INTO burgers (burger_name) VALUES ('bacon cheese burger');
